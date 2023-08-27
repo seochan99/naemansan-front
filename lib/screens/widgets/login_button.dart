0b219084a875509a5e65cc2d7973496cef1c98e4
HuiChan Seo <gmlcks0513@naver.com>
2023-08-27 16:02:03 +0000

    feat: Flutter SDK 방식으로 변경

--- lib/screens/widgets/login_button.dart
+++ lib/screens/widgets/login_button.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:naemansan/screens/Login/webview_apple_screen.dart';
 import 'package:naemansan/screens/Login/webview_google_screen.dart';
 import 'package:naemansan/screens/Login/webview_kakao_screen.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk_talk.dart';
 import 'package:http/http.dart' as http;
 
 class LoginBtn extends StatelessWidget {
@@ -51,6 +52,40 @@ class LoginBtn extends StatelessWidget {
       );
     }
 
+
+    kakaoLoginUseSDK() async {
+      if (await isKakaoTalkInstalled()) {
+        try {
+          await UserApi.instance.loginWithKakaoTalk();
+          // 카카오톡으로 로그인 성공 처리
+        } catch (error) {
+          // 카카오톡으로 로그인 실패 처리
+
+          // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+          // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+          if (error is PlatformException && error.code == 'CANCELED') {
+            return;
+          }
+          // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
+          try {
+            await UserApi.instance.loginWithKakaoAccount();
+            // 카카오계정으로 로그인 성공 처리
+          } catch (error) {
+            // 카카오계정으로 로그인 실패 처리
+          }
+        }
+      } else {
+        try {
+          OAuthToken token = await UserApi.instance.loginWithKakaoAccount();
+          // 카카오계정으로 로그인 성공 처리
+          print("Token : ${token.accessToken}");
+        } catch (error) {
+          // 카카오계정으로 로그인 실패 처리
+        }
+      }
+    }
+
+
     // login function
     void login(logo) async {
       var response = await http.get(
@@ -64,7 +99,8 @@ class LoginBtn extends StatelessWidget {
       String loginUrl = parsedResponse['data']['url'];
       // print("1️⃣ loginURL : $loginUrl");
       if (logo == 'kakao') {
-        kakaoLogin(loginUrl);
+        // kakaoLogin(loginUrl);
+        kakaoLoginUseSDK();
       } else if (logo == 'google') {
         googleLogin(loginUrl);
       } else if (logo == 'apple') {
